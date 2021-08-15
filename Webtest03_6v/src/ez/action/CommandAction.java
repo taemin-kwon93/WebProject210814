d977b6c56ec0bc0be2d10245be51389106344879
DESKTOP-MTGVP5G\DELL <4146kt@gmail.com>
2021-08-15 20:09:25 +0900

    21.08.15 20:09

--- Webtest03_6v/src/ez/action/CommandAction.java
+++ Webtest03_6v/src/ez/action/CommandAction.java
@@ -4,7 +4,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public interface CommandAction {
-	//깃허브 연동 테스트3
 	public String requestPro(HttpServletRequest request, HttpServletResponse response)throws Throwable;
 	
 }
